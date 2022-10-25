2ab7fa9200e3b17c996bf92c18c536d9654d830a
LeonardoSalgado04 <53799801+Salgado2004@users.noreply.github.com>
2022-10-25 18:35:07 -0300

    Update function-slider.js

--- function-slider.js
+++ function-slider.js
@@ -1,126 +1,84 @@
-const questions = document.querySelector("#questions");
-
-function question2(){
-  console.log("Questão2\n");
-  questions.innerHTML = `<section id="question2">
-  <h2>2-  Que feitiço Harry usou para matar Lord Voldemort?</h2>
-  <input id="2option1" type="radio" name="quest2">
-  <label for="2option1">Expelliarmus</label>
-  <input id="2option2" type="radio" name="quest2">
-  <label for="2option2">Patronum Expecto</label>
-  <input id="2option3" type="radio" name="quest2">
-  <label for="2option3">Feitiço da morte</label>
-  <input id="2option4" type="radio" name="quest2">
-  <label for="2option4">Accio</label>
-  <div class="tempo"></div>
-  </section>`;
-  setTimeout(`question3()`, 8000);
+function question(question, opt1, opt2, opt3, opt4, rightAnswer){
+    this.question = question;
+    this.options = {
+        opt1: opt1,
+        opt2: opt2,
+        opt3: opt3,
+        opt4: opt4
+    };
+    this.rightAnswer = rightAnswer;
 }
 
-function question3(){
-    console.log("Questão3\n");
-    questions.innerHTML = `<section id="question3">
-    <h2>3- Qual Horcrux foi a segunda a ser destruída?</h2>
-    <input id="3option1" type="radio" name="quest3">
-    <label for="3option1">O medalhão</label>
-    <input id="3option2" type="radio" name="quest3">
-    <label for="3option2">A taça</label>
-    <input id="3option3" type="radio" name="quest3">
-    <label for="3option3">O anel</label>
-    <input id="3option4" type="radio" name="quest3">
-    <label for="3option4">O diadema</label> 
-    <div class="tempo"></div>
-    </section>`;
-    setTimeout("question4()", 8000);
+
+
+const pergunta1 = new question('O lema "A inteligência além da medida é o maior tesouro do homem" pertence a qual casa', "Grifinória", "Sonserina", "Lufa-lufa", "Corvinal", "Corvinal");
+const pergunta2 = new question("Que feitiço Harry usou para matar Lord Voldemort", "Expelliarmus", "Expecto Patronum", "Feitiço da morte", "Accio", "Expelliarmus");
+const pergunta3 = new question("Qual Horcrux foi a segunda a ser destruída", "O medalhão", "A taça", "O anel", "O diadema", "O medalhão");
+const pergunta4 = new question("Com quem Harry foi ao Baile de Inverno", "Parvati Patil", "Padma Patil", "Cho Chang", "Gina Weasley", "Parvati Patil");
+const pergunta5 = new question("Em qual animal Rita Skeeter se transforma para poder espionar as pessoas", "Um besouro", "Uma formiga", "Uma barata", "Uma aranha", "Um besouro");
+const pergunta6 = new question("Em que ano o primeiro filme foi lançado", "2001", "1999", "2004", "2002", "2001");
+const pergunta7 = new question("A que personagem pertence essa voz", "Remus Lupin", "Gilderoy Lockhart", "Olho-Tonto Moody", "Cornelius Fudge", "Remus Lupin");
+const pergunta8 = new question("Que lugar é esse cenário", "Salão Comunal", "Sala do Dumbledore", "Hogsmeade", "Dedos de mel", "Dedos de mel");
+
+const perguntas = [pergunta1, pergunta2, pergunta3, pergunta4, pergunta5, pergunta6, pergunta7, pergunta8];
+let pontos = 0;
+let acertos = 0;
+
+function showQuestion(id){
+  const questSection = document.querySelector("#questions");
+  if(id < 8){
+    console.log(`Questão ${id+1}\n`);
+    questSection.innerHTML =  `
+    <h2>${id+1}/${perguntas.length}- ${perguntas[id].question}?</h2>
+    <input id="opt1" type="radio" name="quest" onclick="saveAnswer(${id+1}, '${perguntas[id].options.opt1}')">
+    <label for="opt1">${perguntas[id].options.opt1}</label>
+    <input id="opt2" type="radio" name="quest" onclick="saveAnswer(${id+1}, '${perguntas[id].options.opt2}')">
+    <label for="opt2">${perguntas[id].options.opt2}</label>
+    <input id="opt3" type="radio" name="quest" onclick="saveAnswer(${id+1}, '${perguntas[id].options.opt3}')">
+    <label for="opt3">${perguntas[id].options.opt3}</label>
+    <input id="opt4" type="radio" name="quest" onclick="saveAnswer(${id+1}, '${perguntas[id].options.opt4}')">
+    <label for="opt4">${perguntas[id].options.opt4}</label>
+    <div class="tempo"></div>`;
+    setTimeout(`showQuestion(${id+1})`, 10000);
+  }
+  else{
+    endQuiz();
+  }
 }
 
-function question4(){
-    console.log("Questão4\n");
-    questions.innerHTML = `<section id="question4">
-    <h2>4- Harry foi com quem ao Baile de Inverno?</h2>
-    <input id="4option1" type="radio" name="quest4">
-    <label for="4option1">Parvati Patil</label>
-    <input id="4option2" type="radio" name="quest4">
-    <label for="4option2">Padma Patil</label>
-    <input id="4option3" type="radio" name="quest4">
-    <label for="4option3">Cho Chang</label>
-    <input id="4option4" type="radio" name="quest4">
-    <label for="4option4">Gina Weasley</label> 
-    <div class="tempo"></div>
-    </section>`;
-    setTimeout("question5()", 8000);
-}
-
-function question5(){
-    console.log("Questão5\n");
-    questions.innerHTML = `<section id="question5">
-    <h2>5-Rita Skeeter se transforma em qual animal para poder espionar as pessoas?</h2>
-    <input id="5option1" type="radio" name="quest5">
-    <label for="5option1">Um besouro</label>
-    <input id="5option2" type="radio" name="quest5">
-    <label for="5option2">Uma formiga</label>
-    <input id="5option3" type="radio" name="quest5">
-    <label for="5option3">Uma barata</label>
-    <input id="5option4" type="radio" name="quest5">
-    <label for="5option4">Uma aranha</label> 
-    <div class="tempo"></div>
-    </section>`;
-    setTimeout("question6()", 8000);
-}
-
-function question6(){
-    console.log("Questão6\n");
-    questions.innerHTML = `<section id="question6">
-    <h2>6- Em que ano o primeiro filme foi lançado?</h2>
-    <input id="6option1" type="radio" name="quest6">
-    <label for="6option1">2001</label>
-    <input id="6option2" type="radio" name="quest6">
-    <label for="6option2">1999</label>
-    <input id="6option3" type="radio" name="quest6">
-    <label for="6option3">2004</label>
-    <input id="6option4" type="radio" name="quest6">
-    <label for="6option4">2002</label>
-    <div class="tempo"></div>
-    </section>`;
-    setTimeout("question7()", 8000);
-}
-
-function question7(){
-    console.log("Questão7\n");
-    questions.innerHTML = `<section id="question7">
-    <h2>7- Quem tem essa voz?- áudio</h2>
-    <input id="7option1" type="radio" name="quest7">
-    <label for="7option1">Remus Lupin</label>
-    <input id="7option2" type="radio" name="quest7">
-    <label for="7option2">Gilderoy Lockhart</label>
-    <input id="7option3" type="radio" name="quest7">
-    <label for="7option3">Olho-Tonto Moody</label>
-    <input id="7option4" type="radio" name="quest7">
-    <label for="7option4">Cornelius Fudge</label>
-    <div class="tempo"></div>
-    </section>`;
-    setTimeout("question8()", 8000);
-}
-
-function question8(){
-    console.log("Questão8\n");
-    questions.innerHTML = `<section id="question8">
-    <h2>8- Que lugar é esse cenário? </h2>
-    <input id="8option1" type="radio" name="quest8">
-    <label for="8option1">Salão Comunal</label>
-    <input id="8option2" type="radio" name="quest8">
-    <label for="8option2">Sala do Dumbledore</label>
-    <input id="8option3" type="radio" name="quest8">
-    <label for="8option3">Hogsmeade</label>
-    <input id="8option4" type="radio" name="quest8">
-    <label for="8option4">Dedos de mel</label>
-    <div class="tempo"></div>
-    </section>`;
+function saveAnswer(quest, value){
+    const inputs = document.querySelectorAll("#questions input[type='radio']");
+    const labels = document.querySelectorAll("#questions label");
+    for(radio of inputs){
+        radio.onclick = "";
+    }
+    for(label of labels){
+        if(label.innerText == perguntas[quest-1].rightAnswer){
+            label.style.backgroundColor = "green";
+        } else {
+            label.style.backgroundColor = "red";
+        }
+    }
+    if (value == perguntas[quest-1].rightAnswer){
+        console.log(`Questão ${quest}: ${value} - Acertou!`);
+        acertos += 1;
+        pontos += 125;
+    } else{
+        console.log(`Questão ${quest}: ${value} - Errou!`);
+    }
 }
 
 function startQuiz(){
-    console.log("O quiz começou\n");
-    console.log("Questão1\n");
-    setTimeout(`question2()`, 8000);
+    console.log("O Quiz começou\n");
+    showQuestion(0);
+}
+
+function endQuiz(){
+    console.log("Acabou");
+    const questSection = document.querySelector("#questions");
+    questSection.innerHTML = `
+    <h2>Você acertou ${acertos} de ${perguntas.length} perguntas</h2>
+    <h2>Sua pontuação final é ${pontos} pts</h2>
+    `;
 }
 
