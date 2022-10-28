f380f727ee03fd7d588f20425850c8fc0b2b504c
LeonardoSalgado04 <53799801+Salgado2004@users.noreply.github.com>
2022-10-28 00:53:28 -0300

    Update functionQuiz.js

--- functionQuiz.js
+++ functionQuiz.js
@@ -6,9 +6,9 @@ function question(question, opt1, opt2, opt3, opt4, rightAnswer, bonus, img){
     this.img = img;
 }
 
-  function randOrd() {
+function randOrd() {
     return (Math.round(Math.random())-0.5);
-  }
+}
 
 const pergunta1 = new question('O lema "A inteligência além da medida é o maior tesouro do homem" pertence a qual casa', "Grifinória", "Sonserina", "Lufa-lufa", "Corvinal", "Corvinal", "", "img/casas.png");
 const pergunta2 = new question("Que feitiço Harry usou para matar Lord Voldemort", "Expelliarmus", "Expecto Patronum", "Feitiço da morte", "Accio", "Expelliarmus", "", "img/batalhaFinal.jpg");
@@ -20,7 +20,11 @@ const pergunta7 = new question("De que personagem é essa voz", "Remus Lupin", "
 const pergunta8 = new question("Que lugar é esse cenário", "Salão Comunal", "Salão Principal", "Sala de aula", "Gringotts", "Salão Principal", "", "img/grandeSalao.jpg");
 const pergunta9 = new question("Em que ano Voldemort tentou matar o Harry bebê", "1997", "1981", "1998", "1995", "1998", "", "img/bebeHarry.jpg");
 const pergunta10 = new question("Em qual filme/livro o Voldemort retornou", "primeiro", "Quarto", "Segundo", "Sétimo", "Quarto", "", "img/voldemort.jpg");
-let perguntas = [pergunta1, pergunta2, pergunta3, pergunta4, pergunta5, pergunta6, pergunta7, pergunta8, pergunta9, pergunta10];
+const pergunta11 = new question("Quem escreveu Harry Potter", "J.K.Rowling", "Donald Mustard", "Jeff Kinney", "Collen Hoover", "J.K.Rowling", "", "img/livro.jpg");
+const pergunta12 = new question("De que personagem é essa voz", "Remus Lupin", "Sirius Black", "Olho-Tonto Moody", "Severus Snape", "Remus Lupin", "<audio controls autoplay><source src='audio/remus.mp3' type='audio/mpeg'></audio>", "img/voz.png");
+const pergunta13 = new question("De que personagem é essa voz", "Hermione Granger", "Bellatrix", "Gina Weasley", "Luna Lovegood", "Luna Lovegood", "<audio controls autoplay><source src='audio/luna.mp3' type='audio/mpeg'></audio>", "img/voz.png");
+const pergunta14 = new question("Qual o primeiro nome do fundador da casa Sonserina", "Godric", "Salazar", "Lupin", "Hodes", "Salazar", "", "img/sonserina.jpg");
+let perguntas = [pergunta1, pergunta2, pergunta3, pergunta4, pergunta5, pergunta6, pergunta7, pergunta8, pergunta9, pergunta10, pergunta11, pergunta12, pergunta13, pergunta14];
 let pontos = 0;
 let acertos = 0;
 let timer = 0;
@@ -79,6 +83,8 @@ function saveAnswer(quest, value){
 }
 
 function startQuiz(){
+    const regras = document.querySelector(".info_box");
+    document.body.removeChild(regras);
     console.log("O Quiz começou\n");
     perguntas = perguntas.sort(randOrd)
     showQuestion(0);
@@ -89,12 +95,40 @@ function endQuiz(){
     const questSection = document.querySelector("#questions");
     questSection.innerHTML = `
     <div id="FinalPoints"><h2>Você acertou ${acertos} de 8 perguntas</h2>
-    <h2>Sua pontuação final é ${pontos} pts</h2></div>
+    <h2>Sua pontuação final é ${pontos} pts</h2><br><br>
+    <form action="save/" autocomplete="off">
+      <label>Insira seu nome para salvar a pontuação!</label>
+      <input id="nome" type="text" placeholder="Insira seu nome"/>
+      <div>
+      <button type="submit" disabled>Salvar</button>
+      <a href="pontuacoes.html">Ver melhores pontuações</a>
+      </div>
+    </form>
+    </div>
     `;
+    const input = document.querySelector("#nome")
+    input.addEventListener("input", function (event){
+      let button = document.querySelector("form button");
+      button.disabled = false;
+    });
+
+    const form = document.querySelector("form");
+    form.addEventListener("submit", function (event){
+      event.preventDefault();
+      let name = document.querySelector("#nome").value;
+      window.sessionStorage.setItem("nome", name);
+      let nome = sessionStorage.getItem("nome");
+      let pontuacoes = sessionStorage.getItem("pontuacoes");
+      if (pontuacoes == null){
+        window.sessionStorage.setItem("pontuacoes", `${nome}/${pontos}\n`);
+      } else{
+        window.sessionStorage.setItem("pontuacoes", `${pontuacoes}${nome}/${pontos}\n`);
+      }
+      alert("Pontuação salva!!");
+    })
 }
 
 function nextQuest(quest){
   clearTimeout(timer);
   showQuestion(quest);
 }
-
